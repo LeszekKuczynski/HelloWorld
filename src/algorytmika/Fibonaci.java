b35a07be1122b944bff75d700d98a3eff70e5509
Leszek Kuczynski <leszek_kuczynski@outlook.com>
2018-10-07 15:08:51 +0200

    fibonacci numbers sequence of given number 'n'.

---
+++ src/algorytmika/Fibonaci.java
@@ -0,0 +1,25 @@
+package algorytmika;
+
+import java.util.Scanner;
+
+public class Fibonaci {
+    public static void main(String[] args) {
+        System.out.println("---Ciag fibonaciego");
+        int n = new Scanner(System.in).nextInt();
+        
+        int result = fibonaci(n);
+        System.out.println(result);
+    }
+
+    private static int fibonaci(int n) {
+        int f0 = 0;
+        int f1=1;
+
+        if(n==0)
+            return 0;
+        else if (n==1)
+            return 1;
+        else
+            return fibonaci(n-1) + fibonaci( n-2);
+    }
+}
