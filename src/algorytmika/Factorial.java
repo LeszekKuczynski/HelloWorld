4dec384da081ebda89b2267125bc87a797c62b9c
Leszek Kuczynski <leszek_kuczynski@outlook.com>
2018-10-07 11:46:21 +0200

    improved silnia(n)

---
+++ src/algorytmika/Factorial.java
@@ -0,0 +1,34 @@
+/*
+exapmle:
+0! = 1
+1! = 1
+2! = 2
+3! = 6
+...
+n! = n * factor(n-1)
+5! = 5 * factor(5-1) = 5 * factor(4 * factor(4-1)) =
+                        5 * factor(4 * factor(3* factor(3-1))) =
+                        5 * factor(4 * factor(3* factor(2 * factor(2-1)))) =
+                        5 * 4 * 3 * 2 * 1 =
+                        120
+*/
+
+package algorytmika;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println("---Calculate factorial----");
+        System.out.print("n: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int product = factorial(n);
+        System.out.println("Product of all: " + product);
+    }
+
+    private static int factorial(int n) {
+        if (n <= 1) return 1;
+        return n * factorial(n - 1);
+    }
+}
