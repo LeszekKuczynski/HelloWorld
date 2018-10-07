9c33eadc0210703c2fed075ef7b71f66cc4ba12d
Leszek Kuczynski <leszek_kuczynski@outlook.com>
2018-10-07 11:25:16 +0200

    improve silnia(n)

--- src/algorytmika/Silnia.java
+++ src/algorytmika/Silnia.java
@@ -17,8 +17,7 @@ public class Silnia {
     }
 
     private static int factorial(int n) {
-        if (n <= 1)
-            return 1;
+        if (n <= 1) return 1;
         return n * factorial(n - 1);
 
 
