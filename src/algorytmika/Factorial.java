c7f8d4f1f06676185dc5c443562bdbdcf31162f8
Leszek Kuczynski <leszek_kuczynski@outlook.com>
2018-10-07 12:25:23 +0200

    Calculate product of all 'n' in factorial(n).

--- src/algorytmika/Factorial.java
+++ src/algorytmika/Factorial.java
@@ -27,7 +27,8 @@ public class Factorial {
     }
 
     private static int factorial(int n) {
-        if (n <= 1) return 1;
-        return n * factorial(n - 1);
+        if (n>1)
+            return n * factorial(n - 1);
+        return 1;
     }
 }
