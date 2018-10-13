0617d2393f873cad957bc03c2d316f0bf289bd88
Leszek Kuczynski <leszek_kuczynski@outlook.com>
2018-10-13 19:34:56 +0200

    Print four shapes on console screen from given input: number. Number is height and width of shape. Print triangle, diagonal, Square and DaoubleDiagonal. Esample input: 6 *      *       ******  *     * **      *      *    *   *   * ***      *     *    *    * * ****      *    *    *    * * *****      *   *    *   *   * ******      *  ******  *     *

--- src/algorytmika/Excercises1/PrintFigures.java
+++ src/algorytmika/Excercises1/PrintFigures.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class PrintFigures {
     public static void main(String[] args) {
-        //
         System.out.println("==Draw 4 figures. Give number of hight and wight: ");
         int n = new Scanner(System.in).nextInt();
 
